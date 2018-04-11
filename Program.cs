35bd2ef1f0257ce5162fe8623bb80440d3a8b72a
Anu Thomas Chandy <anuamd@hotmail.com>
2018-04-10 22:01:00 -0700

    2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
 
 namespace EMSIDotNet
 {
@@ -6,7 +11,43 @@ namespace EMSIDotNet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program program = new Program();
+            program.FooAsync(CancellationToken.None).Wait();
+        }
+
+        public async Task FooAsync(CancellationToken cancellationToken)
+        {
+
+            var builder = new UriBuilder("http://169.254.169.254/metadata/identity/oauth2/token");
+            var query = HttpUtility.ParseQueryString(builder.Query);
+            query["api-version"] = "2018-02-01";
+            query["resource"] = "https://management.azure.com/";
+            query["msi_res_id"] = "/subscriptions/ec0aa5f7-9e78-40c9-85cd-535c6305b380/resourcegroups/1c2b4bf7e1f448e/providers/Microsoft.ManagedIdentity/userAssignedIdentities/msi-idd7b42487";
+            builder.Query = query.ToString();
+            string url = builder.ToString();
+
+            HttpRequestMessage msiRequest = new HttpRequestMessage(HttpMethod.Get, url);
+            msiRequest.Headers.Add("Metadata", "true");
+
+            var msiResponse = await (new HttpClient()).SendAsync(msiRequest, cancellationToken);
+            string content = await msiResponse.Content.ReadAsStringAsync();
+            dynamic loginInfo = JsonConvert.DeserializeObject(content);
+
+            string accessToken = loginInfo.access_token;
+            if (accessToken != null)
+            {
+                Console.WriteLine($"access_token: {accessToken}");
+            }
+            string expiresOn = loginInfo.expires_on;
+            if (expiresOn != null)
+            {
+                Console.WriteLine($"expires_on: {expiresOn}");
+            }
+            string tokenType = loginInfo.token_type;
+            if (tokenType != null)
+            {
+                Console.WriteLine($"token_type: {tokenType}");
+            }
         }
     }
 }
